8d0599bc4258e4a786ebf0e12141bebaba01b191
Benjamin Leray <b.leray35@gmail.com>
2021-03-10 18:28:39 +0100

    clean up and mock communication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [response, setResponse] = useState('')
+
+  useEffect( () =>{
+    const testConnection = async () => {
+      const response = await fetch('http://localhost:3000/api/test/connection')
+      setResponse(response.content)
+    };
+    testConnection();
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> Soundtrace - Web </h1>
+     <p>{response}</p>
     </div>
   );
-}
+};
 
 export default App;
