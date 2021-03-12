fb237f3dc34623d1a35407e2c62444f703240577
Benjamin Leray <b.leray35@gmail.com>
2021-03-12 11:54:52 +0100

    fix compiled errors

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,12 @@ import serverFetch from './api/axios';
 
 import './App.css';
 
-const cou_cou = 'coucou'
-
 const App = () => {
   const testConnection = async () => {
     const response = await serverFetch.post('/auth/sign_in', {
       email: 'test1@gmail.com',
       password: 'password',
     });
-    console.log("erf")
     return response;
   };
 
@@ -22,12 +19,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>
-
-
-        Soundtrace - Web
-      </h1>
-      <div>{cou_cou}</div>
+      <h1> Soundtrace - Web </h1>
     </div>
   );
 };
